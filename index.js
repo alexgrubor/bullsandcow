273ca42329928aa43550512e110a8083812ef127
Alex <alexgrubor@gmail.com>
2022-11-14 14:16:43 +0100

    version 1 done

--- index.js
+++ index.js
@@ -3,9 +3,54 @@
 // we need to keep the next line, so we can prompt the user for input
 const rs = require('readline-sync');
 
-// Test that prompt is working 
-let name = rs.question('What is your name? ');
-console.log(`User's input is: ${name}`);
+let secretNumber = () => {
+  let result = [];
+  do {
+    let randomNum = Math.floor(Math.random() * 10);
+    if (!result.includes(randomNum)) {
+      result.push(randomNum);
+    }
+  } while (result.length < 4);
 
- // Feel free to edit / remove the line above, this is just to test the package
-//  Although we may want to use the user's name for something 
+  return (result.join(""));
+};
+let secretGuess = secretNumber();
+
+console.log('secretGuess :>> ', secretGuess);
+let userGuess = rs.question('Please enter guessed Number: ');
+
+let checkUserUniqueInput = () => {
+    let someObj = {}
+    for (let i = 0; i <userGuess.length; i++){
+        let char = userGuess[i]
+        if(someObj[char]) return false;
+        someObj[char] = true;
+    }
+    return true;
+}
+
+console.log('userGuess :>> ', userGuess);
+let checkUserInput = () => {
+if (userGuess === '' || userGuess.length <4 || !checkUserUniqueInput()) {
+    console.log('Error :>> ', 'Error');
+ }
+}
+checkUserInput()
+let bullsAndCows = () => {
+ let cows = 0;
+ let bulls = 0;
+
+for (let i = 0; i < userGuess.length; i++) {
+  if (secretGuess[i] === userGuess[i]) {
+    bulls++;
+  } else if (secretGuess.includes(userGuess[i])){
+    cows++;
+  }
+}
+return `cows: ${cows}, bulls ${bulls}`
+}
+
+console.log(bullsAndCows())
+if (secretGuess === userGuess) {
+    console.log('Congratulations')
+}
---
+++ main.js
@@ -0,0 +1,66 @@
+const rs = require("readline-sync");
+
+let secretNumber = () => {
+  let result = [];
+  do {
+    let randomNum = Math.floor(Math.random() * 10);
+    if (!result.includes(randomNum)) {
+      result.push(randomNum);
+    }
+  } while (result.length < 4);
+
+  return result.join("");
+};
+let secretGuess = secretNumber();
+console.log("secretGuess :>> ", secretGuess);
+
+let userGuess = "";
+let playingNumber = 0;
+let startGame = () => {
+  do {
+    userGuess = rs.question("Please enter guessed Number: ");
+    let checkUserUniqueInput = () => {
+      let someObj = {};
+      playingNumber++;
+      for (let i = 0; i < userGuess.length; i++) {
+        let char = userGuess[i];
+        if (someObj[char]) return false;
+        someObj[char] = true;
+      }
+      return true;
+    };
+    if (userGuess === "" || userGuess.length < 4 || !checkUserUniqueInput()) {
+      console.log("Error Error");
+    }
+    let bullsAndCows = () => {
+      let cows = 0;
+      let bulls = 0;
+
+      for (let i = 0; i < userGuess.length; i++) {
+        if (secretGuess[i] === userGuess[i]) {
+          bulls++;
+        } else if (secretGuess.includes(userGuess[i])) {
+          cows++;
+        } 
+      }
+      if (cows === 0 && bulls === 0 ) {
+        return `You missed everything`
+      }
+      return `cows: ${cows}, bulls ${bulls}`;
+    };
+
+    console.log(bullsAndCows());
+    if (secretGuess === userGuess) {
+      console.log(`Congratulations, you guessed with ${playingNumber} guesses`);
+      let again = rs.question("Do you want to play again? Y/N ");
+      if (again.toLowerCase() === "y") {
+        secretGuess = secretNumber();
+        startGame();
+      }
+      if (again.toLowerCase() === "n") {
+        console.log(`Thanks for playing`);
+      }
+    }
+  } while (secretGuess !== userGuess);
+};
+startGame();
